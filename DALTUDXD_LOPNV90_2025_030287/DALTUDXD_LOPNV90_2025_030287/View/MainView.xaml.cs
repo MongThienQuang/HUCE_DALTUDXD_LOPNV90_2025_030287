788077ed83db2c1fe4d3c9a1140270b82a627049
PhungHuuPhu <phunghuuphu2004@gmail.com>
2025-10-20 11:00:55 +0700

    HuuPhu/MainView/Hoàn Thành Sơ Bộộ

---
+++ DALTUDXD_LOPNV90_2025_030287/DALTUDXD_LOPNV90_2025_030287/View/MainView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DALTUDXD_LOPNV90_2025_030287.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtTenLoaiGach_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void dgSan_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
